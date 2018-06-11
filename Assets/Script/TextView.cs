3cbb7d886908dcc5642b90964962e76a49e8978b
V0V <grapduck@gmail.com>
2018-06-11 20:18:36 +0900

    무엇? 문제 무엇
    What is problem?

--- Assets/Script/TextView.cs
+++ Assets/Script/TextView.cs
@@ -8,6 +8,8 @@ public class TextView : MonoBehaviour
 
     public int count;
 
+    public GameObject dialogueWindowPrefab;
+
     public GameObject nameText;
     public GameObject sentenceText;
 
@@ -17,10 +19,16 @@ public class TextView : MonoBehaviour
         {
             count++;
 
+            GameObject dWP = Instantiate(dialogueWindowPrefab) as GameObject;
+            dWP.transform.position = new Vector3(-6, (float)-1.97, -8);
+            dWP.transform.localScale = new Vector3((float)2.4, (float)2.5, 1);
+
             if (count == 1)
             {
                 nameText = GameObject.Find("NameText");
                 nameText.GetComponent<Text>().text = "니시키노 마키";//빠저있던거
+                sentenceText = GameObject.Find("SentenceText");
+                sentenceText.GetComponent<Text>().text = "어라 설마 늦잠, 잤나?";
             }
         }
     }
