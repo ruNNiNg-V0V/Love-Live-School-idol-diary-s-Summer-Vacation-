b968584c2785a0b947fe01432a3992c2fffbad0a
V0V <grapduck@gmail.com>
2018-06-11 19:48:10 +0900

    대사 넘기기
    이름이 안 생김

--- Assets/Script/Click warning scene button method.cs
+++ Assets/Script/Click warning scene button method.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
-public class NewBehaviourScript : MonoBehaviour {
+public class ClickWarningSceneButtonMethod : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
---
+++ Assets/Script/TextView.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TextView : MonoBehaviour
+{
+
+    public int count;
+
+    public GameObject nameText;
+    public GameObject sentenceText;
+
+    void ReplaceTextView()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            count++;
+
+            if (count == 1)
+            {
+                nameText = GameObject.Find("NameText");
+                nameText.GetComponent<Text>().text = "니시키노 마키";//빠저있던거
+            }
+        }
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+}
